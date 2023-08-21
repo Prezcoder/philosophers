470e0da4a0c59d425db75ecefb114991a632583e
Prezcoder <fbouchar@student.42quebec.com>
2023-08-21 09:35:36 -0400

    First commit

---
+++ include/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbouchar <fbouchar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/21 09:15:23 by fbouchar          #+#    #+#             */
+/*   Updated: 2023/08/21 09:17:44 by fbouchar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <pthread.h>
+
+int	main(int argc, char **argv);
+
+#endif
